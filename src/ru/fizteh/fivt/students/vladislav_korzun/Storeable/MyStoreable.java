d2388e2efe4b91a796efa4f034993deff9f69796
Sdernal <sdernal2@gmail.com>
2014-12-02 22:02:15 +0400

    add first class

---
+++ src/ru/fizteh/fivt/students/vladislav_korzun/Storeable/MyStoreable.java
@@ -0,0 +1,70 @@
+package ru.fizteh.fivt.students.vladislav_korzun.Storeable;
+
+import ru.fizteh.fivt.storage.structured.ColumnFormatException;
+import ru.fizteh.fivt.storage.structured.Storeable;
+
+public class MyStoreable implements Storeable {
+
+    @Override
+    public void setColumnAt(int columnIndex, Object value)
+            throws ColumnFormatException, IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Object getColumnAt(int columnIndex) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Integer getIntAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long getLongAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Byte getByteAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Float getFloatAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double getDoubleAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean getBooleanAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getStringAt(int columnIndex) throws ColumnFormatException,
+            IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
